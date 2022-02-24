501f43ef78ad17f2299609cf8bd4c28272698cb1
lirong <lirong_huo@163.com>
2022-02-24 21:00:33 +0800

    imagenet

---
+++ ImageNet/models_imagenet/resnet_EE_square.py
@@ -0,0 +1,261 @@
+import os
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utils.core import Add_Square, CannyFilter_step125_1, HighFreqSuppress, CannyFilter, get_gaussian_kernel
+import torchvision.models
+__all__ = ['ResNet_EE_square', 'resnet18_EE_square', 'resnet34_EE_square', 'resnet50_EE_square', 'resnet101_EE_square', 'resnet152_EE_square']
+
+# you need to download the models to ~/.torch/models
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+models_dir = os.path.expanduser('~/.torch/models')
+model_name = {
+    'resnet18': 'resnet18-5c106cde.pth',
+    'resnet34': 'resnet34-333f7ec4.pth',
+    'resnet50': 'resnet50-19c8e357.pth',
+    'resnet101': 'resnet101-5d3b4d8f.pth',
+    'resnet152': 'resnet152-b121ed2d.pth',
+}
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+class ResNet_EE_square(nn.Module):
+
+    def __init__(self, block, layers, num_classes=1000, cize= 224, r=16, w=0.5, with_gf=False, low=60.0, high=120.0, alpha = 0.0, sigma=1,type_canny='CannyFilter', epsilon=0.05, n_queries=5000):
+        super(ResNet_EE_square, self).__init__()
+        self.inplanes = 64
+        self.w = w
+        self.with_gf = with_gf
+        # hfs and canny
+        # print('r=:' + str(r))
+        # print('w=:' + str(w))
+        # print('Gaussian Filter:' + str(with_gf))
+        self.hfs = HighFreqSuppress(cize, cize, r)
+        # self.canny = CannyFilter(sigma=sigma, use_cuda=True, alpha=alpha)
+        if type_canny == 'CannyFilter':
+            self.canny = CannyFilter(sigma=sigma, use_cuda=True, alpha=alpha)
+        elif type_canny == 'CannyFilter_step125_1':
+            self.canny = CannyFilter_step125_1(sigma=sigma, use_cuda=True, alpha=alpha)
+        else:
+            raise NotImplementedError
+        #square
+        self.add_square = Add_Square(size=cize, epsilon=epsilon, n_queries=n_queries)
+
+
+        self.low = low / 255
+        self.high = high / 255
+
+        k_gaussian = 3
+        gaussian_2D = get_gaussian_kernel(k_gaussian, 0., 1.)
+        gaussian_2D_torch = torch.from_numpy(gaussian_2D).unsqueeze(0).unsqueeze(0).type(torch.float)
+        self.weight_gaussian = nn.Parameter(data=gaussian_2D_torch, requires_grad=False)
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1,
+                          stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        # Add square
+        x_square = self.add_square(x)
+        
+        # Remove high frequency
+        x_hfs = self.hfs(x_square)
+        # Canny
+        x_canny = self.canny(x, low_threshold=self.low, high_threshold=self.high, hysteresis=True)
+        # x = x_canny.type(torch.float)
+        if self.with_gf:
+            x_canny = F.conv2d(x_canny.type(torch.float), self.weight_gaussian, padding=1)
+            x = x_hfs + self.w * x_canny
+        else:
+            x = x_hfs + self.w * x_canny
+
+        x = torch.clamp(x, 0.0, 1.0)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+
+
+def resnet18_EE_square(pretrained=False, **kwargs):
+    """Constructs a ResNet-18 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet_EE_square(BasicBlock, [2, 2, 2, 2], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet18'])))
+    return model
+
+
+def resnet34_EE_square(pretrained=False, **kwargs):
+    """Constructs a ResNet-34 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet_EE_square(BasicBlock, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet34'])))
+    return model
+
+
+def resnet50_EE_square(pretrained=False, **kwargs):
+    """Constructs a ResNet-50 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet_EE_square(Bottleneck, [3, 4, 6, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet50'])))
+    return model
+
+
+def resnet101_EE_square(pretrained=False, **kwargs):
+    """Constructs a ResNet-101 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet_EE_square(Bottleneck, [3, 4, 23, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet101'])))
+    return model
+
+
+def resnet152_EE_square(pretrained=False, **kwargs):
+    """Constructs a ResNet-152 model.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+    """
+    model = ResNet_EE_square(Bottleneck, [3, 8, 36, 3], **kwargs)
+    if pretrained:
+        model.load_state_dict(torch.load(os.path.join(models_dir, model_name['resnet152'])))
+    return model
+
