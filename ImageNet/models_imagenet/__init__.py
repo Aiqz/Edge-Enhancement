aacd8db4109da42679100e9845107e0b6955369b
lirong <lirong_huo@163.com>
2022-02-24 20:46:20 +0800

    imagenet

--- ImageNet/models_imagenet/__init__.py
+++ ImageNet/models_imagenet/__init__.py
@@ -1,3 +1,4 @@
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 from .resnet_EE import resnet18_EE, resnet34_EE, resnet50_EE, resnet101_EE, resnet152_EE
 from .resnet_fd import resnet18_fd, resnet34_fd, resnet50_fd, resnet101_fd, resnet152_fd
+from .resnet_EE_square import resnet101_EE_square, resnet152_EE_square, resnet18_EE_square, resnet34_EE_square,resnet50_EE_square
